39f0e4f780046471d191cb96df762131c37dd081
Christopher Monsanto <chris@monsan.to>
2020-04-13 00:50:49 -0400

    Add back models

--- Tupfile.lua
+++ Tupfile.lua
@@ -9,6 +9,11 @@ for file in iglob{"src/models/front/*", "src/models/front-cosmetic/*"} do
     tup.rule(file, "ln -s ../../../%f %o", "build/ps/ani/" .. output)
 end
 
+for file in iglob{"src/models/back/*", "src/models/back-cosmetic/*"} do
+    local output = toPSSpriteID(decodeFS(tup.base(file))) .. "." .. tup.ext(file)
+    tup.rule(file, "ln -s ../../../%f %o", "build/ps/ani-back/" .. output)
+end
+
 for file in iglob{"src/models/front/*"} do
     local output = toSmogonAlias(decodeFS(tup.base(file))) .. "." .. tup.ext(file)
     tup.rule(file, "ln -s ../../../%f %o", "build/smogon/xy/" .. output)
