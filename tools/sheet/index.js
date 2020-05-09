ea2b48e77fff865e90251b4661f65ccbec80fd3b
Christopher Monsanto <chris@monsan.to>
2020-05-09 07:36:28 -0400

    Allow gaps in sheets

--- tools/sheet/index.js
+++ tools/sheet/index.js
@@ -13,6 +13,15 @@ if (!sheetjs || !dest) {
 (async() => {
     const {default: sheet} = await import(path.join(process.cwd(), sheetjs));
 
+    for (let i = 0; i < sheet.entries.length; i++) {
+        if (sheet.entries[i] === undefined)
+            throw new Error(`gap: ${i}`);
+        if (sheet.entries[i] === null) {
+            // ImageMagick blank entry
+            sheet.entries[i] = "xc:transparent";
+        }
+    }
+
     cp.execFileSync("montage", [
         ...sheet.entries,
         "-background", "transparent",
