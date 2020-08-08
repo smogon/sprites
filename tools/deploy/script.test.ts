7b9373d3c7378bf88726d4275f15be70c3bf517b
Christopher Monsanto <chris@monsan.to>
2020-08-08 05:43:50 -0400

    deploy/script: add small test for run()

--- tools/deploy/script.test.ts
+++ tools/deploy/script.test.ts
@@ -2,10 +2,24 @@
 import * as pathlib from './path.js';
 import * as script from './script.js';
 
-test('correct output', () => {
+test('runOnFile', () => {
     const scr = new script.Script('({name: "25"})');
     const src = pathlib.path('/foo/bar/pikachu.png');
     const dst = scr.runOnFile(src);
     expect(dst).toEqual(pathlib.path('25.png'));
+});
+
+test('run identity', () => {
+    const aq = new script.ActionQueue();
+    const scr = new script.Script(`
+for (const p of list(".")) {
+    copy(p, p);
+}
+    `);
+    scr.run("testsrc", "testdst", aq);
+    expect(aq.describe()).toEqual(expect.arrayContaining([
+        {src: pathlib.path('testsrc/32.png'), dst: pathlib.path("testdst/32.png")},
+        {src: pathlib.path('testsrc/192-g-vsmogon.png'), dst: pathlib.path("testdst/192-g-vsmogon.png")},
+    ]));
 });
 
