8b6c8586115098e287d84f36a9064f5d5e1aebff
Christopher Monsanto <chris@monsan.to>
2020-08-15 02:15:21 -0400

    Now that we're TS, move back to commonjs for jest

--- data/lib/index.ts
+++ data/lib/index.ts
@@ -3,8 +3,6 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 const libdir = path.join(__dirname, "../..");
 
 export type Type = {
