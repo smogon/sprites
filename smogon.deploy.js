e4d174f5c5aa8f0bf7ab6cda2a28139d953a0573
Christopher Monsanto <chris@monsan.to>
2020-08-23 22:09:41 -0400

    in-progress port of smogon.deploy.js to new DSL

--- smogon.deploy.js
+++ smogon.deploy.js
@@ -1,16 +1,46 @@
 
-transform(toSmogonSpriteAlias, () => {
-    // dest("forumsprites");
-    // sel(
-    //     "build/gen6-minisprites-padded/noncanonical/pokemon",
-    //     "build/gen6-minisprites-padded/canonical/pokemon",
-    //     "build/gen6-minisprites-padded/cap/pokemon"
-    // );
+function toSmogonAlias(name) {
+    return name.toLowerCase().
+        replace(/[ _]+/, "-").
+        replace(/[^a-z0-9-]+/g, '');
+}
 
-    // dest("fbsprites/xy");
-    // sel("build/smogon/fbsprites/xy");
+function spritecopy(f, {dir, ext}) {
+    const sn = spritename.parse(f.name);
+    let name;
 
-    // dest("twittersprites/xy");
-    // sel("build/smogon/twittersprites/xy");
-});
+    // Skip asymmetrical for now
+    if (sn.extra.has("a")) {
+        return;
+    }
+    
+    if (typeof sn.id === 'string') {
+        name = toSmogonAlias(sn.id);
+    } else {
+        const sd = spritedata.get(sn.id);
+        name = toSmogonAlias(sd.base);
+        if (sd.forme) {
+            name += `-${toSmogonAlias(sd.forme)}`;
+        }
+    }
+    if (sn.extra.has("f")) {
+        name += "-f";
+    }
+    if (sn.extra.has("g")) {
+        name += "-gmax";
+    }
+    
+    copy(f, {dir, ext, name});
+}
 
+for (const f of list("build/gen6-minisprites-padded")) {
+    spritecopy(f, {dir: "forumsprites"});
+}
+
+for (const f of list("build/smogon/fbsprites/xy")) {
+    spritecopy(f, {dir: "fbsprites/xy"});
+}
+
+for (const f of list("build/smogon/twittersprites/xy")) {
+    spritecopy(f, {dir: "twittersprites/xy"});
+}
