1ac4104be54ca6b13c6d011a8b809b9c9379717b
Christopher Monsanto <chris@monsan.to>
2020-09-02 01:52:39 -0400

    sheet: use top-level await

--- tools/sheet/index.ts
+++ tools/sheet/index.ts
@@ -10,26 +10,23 @@ if (!sheetjs || !dest) {
     throw new Error(`node tools/sheet <file.sheet.js> <dest>`);
 }
 
-// Top-level await?
-(async() => {
-    const {default: sheet} = await import(path.join(process.cwd(), sheetjs));
+const {default: sheet} = await import(path.join(process.cwd(), sheetjs));
 
-    for (let i = 0; i < sheet.entries.length; i++) {
-        if (sheet.entries[i] === undefined)
-            throw new Error(`gap: ${i}`);
-        if (sheet.entries[i] === null) {
-            // ImageMagick blank entry
-            sheet.entries[i] = "xc:transparent";
-        }
+for (let i = 0; i < sheet.entries.length; i++) {
+    if (sheet.entries[i] === undefined)
+        throw new Error(`gap: ${i}`);
+    if (sheet.entries[i] === null) {
+        // ImageMagick blank entry
+        sheet.entries[i] = "xc:transparent";
     }
+}
 
-    cp.execFileSync("montage", [
-        ...sheet.entries,
-        "-background", "transparent",
-        "-geometry", `${sheet.width}x${sheet.height}>`,
-        "-gravity", "center",
-        "-tile", `${sheet.tile}x`,
-        "-depth", "8",
-        dest
-    ]);
-})();
+cp.execFileSync("montage", [
+    ...sheet.entries,
+    "-background", "transparent",
+    "-geometry", `${sheet.width}x${sheet.height}>`,
+    "-gravity", "center",
+    "-tile", `${sheet.tile}x`,
+    "-depth", "8",
+    dest
+]);
